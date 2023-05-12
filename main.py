ae69b0824f42417067edb89e0aab157df6002dfe
rimgro <rimgro.mail@gmail.com>
2023-05-12 19:10:01 +0300

    Добавлен конфиг

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-from flask import Flask, request
+import subprocess
 
+from flask import Flask, request
+import json
+
+
+
+def execute_command(command):
+    try:
+        subprocess.check_output(command, shell=True)
+        print('Command executed successfully')
+    except subprocess.CalledProcessError as e:
+        print('Error executing command:', e.output)
+
+
+# Загрузка данных из файла конфига
+with open('config.json') as file:
+    config_data = json.load(file)
+
+port = config_data.get("port", 12345)
+webhook_route = config_data.get("webhook_route", "/webhook")
+action = config_data.get("action", [])
 
 app = Flask(__name__)
 
-@app.route('/testwebhook', methods=['POST'])
+
+@app.route(webhook_route, methods=['POST'])
 def webhook():
     if request.method == 'POST':
         payload = request.json  # Получаем данные из запроса
         # Выполните необходимые действия здесь, например, выполните команду git pull
         # для загрузки последних изменений репозитория
-        print(payload)
-        return 'Webhook received successfully'  # Ответите на запрос от GitHub
+        print(fr"Получен вебхук из репозитория {payload['repository']['name']} от {payload['pusher']['name']}")
+        execute_command(action)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=12354)
+    app.run(host='0.0.0.0', port=port)
